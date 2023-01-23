bfe64ca56bd562151e4f0f14e1aaa38943a63274
puneetanimator <puneetanimator@gmail.com>
2023-01-23 19:37:34 +0530

    Negotiation Basics

---
+++ story_content/user.js
@@ -0,0 +1,131 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5YCIZGfLD6c":
+        Script1();
+        break;
+      case "6ZB7Sg4KAxw":
+        Script2();
+        break;
+      case "5kzpDtG0StB":
+        Script3();
+        break;
+      case "6HpOPenTWc5":
+        Script4();
+        break;
+  }
+}
+
+function Script1()
+{
+  var myCode = function() {
+	$( ".menu-icon-wrapper" ).click();
+}
+if (window.$ != null) { // If jQuery has already loaded, run myCode.
+
+	$( ".menu-icon-wrapper" ).click();
+
+} else { // Else, load jQuery and then run myCode.
+
+	var jQueryLoader = document.createElement("script");
+
+	jQueryLoader.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js";
+
+	jQueryLoader.onload = function() {
+
+		jQueryLoader.onload = null;
+
+		$( ".menu-icon-wrapper" ).click();
+
+	}
+
+	document.head.appendChild(jQueryLoader);
+
+}
+}
+
+function Script2()
+{
+  var myCode = function() {
+	$( ".menu-icon-wrapper" ).click();
+}
+if (window.$ != null) { // If jQuery has already loaded, run myCode.
+
+	$( ".menu-icon-wrapper" ).click();
+
+} else { // Else, load jQuery and then run myCode.
+
+	var jQueryLoader = document.createElement("script");
+
+	jQueryLoader.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js";
+
+	jQueryLoader.onload = function() {
+
+		jQueryLoader.onload = null;
+
+		$( ".menu-icon-wrapper" ).click();
+
+	}
+
+	document.head.appendChild(jQueryLoader);
+
+}
+}
+
+function Script3()
+{
+  var myCode = function() {
+	$( ".menu-icon-wrapper" ).click();
+}
+if (window.$ != null) { // If jQuery has already loaded, run myCode.
+
+	$( ".menu-icon-wrapper" ).click();
+
+} else { // Else, load jQuery and then run myCode.
+
+	var jQueryLoader = document.createElement("script");
+
+	jQueryLoader.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js";
+
+	jQueryLoader.onload = function() {
+
+		jQueryLoader.onload = null;
+
+		$( ".menu-icon-wrapper" ).click();
+
+	}
+
+	document.head.appendChild(jQueryLoader);
+
+}
+}
+
+function Script4()
+{
+  var myCode = function() {
+	$( ".menu-icon-wrapper" ).click();
+}
+if (window.$ != null) { // If jQuery has already loaded, run myCode.
+
+	$( ".menu-icon-wrapper" ).click();
+
+} else { // Else, load jQuery and then run myCode.
+
+	var jQueryLoader = document.createElement("script");
+
+	jQueryLoader.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js";
+
+	jQueryLoader.onload = function() {
+
+		jQueryLoader.onload = null;
+
+		$( ".menu-icon-wrapper" ).click();
+
+	}
+
+	document.head.appendChild(jQueryLoader);
+
+}
+}
+
